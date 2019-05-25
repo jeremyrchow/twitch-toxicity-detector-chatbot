0aee2280788967eeab5d3d27256fbce1a0d2e7f2
Jeremy C <jchow@Jeremys-MacBook-Pro.local>
2019-05-25 12:51:34 -0700

    Commenting bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ const tmi = require('tmi.js'); // for
 var http = require('http'); //for HTTP Posts
 var request = require('request');
 var fs = require('fs');
+
+// HEROKU syntax for passing twitch account user auth key
 var oauth_key =  process.env.TWITCH_OAUTH_KEY;
 //var oauth_key = fs.readFileSync('./oauth_key/key.txt').toString();
 
@@ -18,8 +20,9 @@ const opts = {
     'datatestdummy'
   ]
 };
-
+// Set model URL to send classification requests to
 const model_URL = 'https://chat-toxicity-classifier.herokuapp.com/?chat_in=yeah'
+
 // Create a client with our options
 const client = new tmi.client(opts);
 
@@ -30,18 +33,21 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
-
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   //  const commandName = msg.trim();
+  // ^ This step is not necessary, handled by website
   
   console.log("\nInput Message: " + msg)
   
   const commandName = msg
   
+  // Send a post request to model URL. Website will handle post request, and send JSON with
+  // rows of format {{'name' = 'toxic_category' : 'probability' = value}}
+
   var message_to_print = "";
   request.post({
     url:      model_URL,
@@ -68,15 +74,9 @@ function onMessageHandler (target, context, msg, self) {
     }
   );
 
-  // Stuff out here doesn't happen on message
+  // Stuff out here doesn't happen on message received
   //console.log("Message2 is: " + message_to_print);
   
-}
-
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
 }
 
 // Called every time the bot connects to Twitch chat
